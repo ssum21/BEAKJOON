fb0690d9a0e82dc65bfaf8943795093640646b24
ssum21 <ssumuss@khu.ac.kr>
2022-11-30 18:37:46 +0900

    221130

---
+++ 2206.py
@@ -0,0 +1,14 @@
+N,M=input().split()
+N=int(N)
+M=int(M)
+arr=[]
+for i in range(N):
+    arr.append(list(map(int,input())))
+    
+    for j in range(M):
+        arr[i].append(list)
+    
+
+print(arr)
+hit=1
+
