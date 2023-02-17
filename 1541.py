6b9ca745350c3cfc7e4d8f1c17e00630ca425dcf
ssum21 <ssumuss@khu.ac.kr>
2023-02-17 10:00:35 +0900

    Problem Sovling 1541 (number of challenges : 1)

---
+++ 1541.py
@@ -0,0 +1,49 @@
+'''Problem
+세준이는 양수와 +, -, 그리고 괄호를 가지고 식을 만들었다. 그리고 나서 세준이는 괄호를 모두 지웠다.
+그리고 나서 세준이는 괄호를 적절히 쳐서 이 식의 값을 최소로 만들려고 한다.
+괄호를 적절히 쳐서 이 식의 값을 최소로 만드는 프로그램을 작성하시오.
+
+첫째 줄에 식이 주어진다. 식은 ‘0’~‘9’, ‘+’, 그리고 ‘-’만으로 이루어져 있고, 가장 처음과 마지막 문자는 숫자이다. 그리고 연속해서 두 개 이상의 연산자가 나타나지 않고, 5자리보다 많이 연속되는 숫자는 없다. 수는 0으로 시작할 수 있다. 입력으로 주어지는 식의 길이는 50보다 작거나 같다.
+'''
+
+import sys
+a = str(input())
+num=0
+tot=0
+minus=False
+temp='0'
+for i in range(len(a)):
+    if(a[i]!='+' and a[i]!='-'):
+        temp+=a[i]
+    else:
+        temp=int(temp)
+        num=temp
+        temp='0'
+        if(minus):
+            tot-=num
+        else:
+            tot+=num
+        if(a[i]=='-'):
+            minus=True
+
+if(minus):
+    tot-=int(temp)
+else:
+    tot+=int(temp)
+
+print(tot)
+
+''' Example Code
+a = input().split('-')
+num = []
+for i in a:
+    cnt = 0
+    s = i.split('+')
+    for j in s:
+        cnt += int(j)
+    num.append(cnt)
+n = num[0]
+for i in range(1, len(num)):
+    n -= num[i]
+print(n)
+'''
