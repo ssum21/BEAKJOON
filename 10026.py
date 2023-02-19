5880164688327a07a98c1be18104c676032ea8be
ssum21 <ssumuss@khu.ac.kr>
2023-02-19 14:28:59 +0900

    BFS Problem Practice

---
+++ 10026.py
@@ -0,0 +1,17 @@
+num=int(input())
+
+visted=[[] for i in range(num)]
+    
+
+def DFS(N):
+    q=[]
+    q.append(N)
+    visted[i][j]=1
+    
+
+
+li=['' for i in range(num)]
+
+for i in range(num):
+    li[i]=input()
+
