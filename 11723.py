e74f0089deec0a10950aea2cdfef616abdf266e3
ssum21 <ssumuss@khu.ac.kr>
2023-02-26 23:46:37 +0900

    Problem Solving 11723

--- 11723.py
+++ 11723.py
@@ -1,23 +1,49 @@
+''' Problem
+비어있는 공집합 S가 주어졌을 때, 아래 연산을 수행하는 프로그램을 작성하시오.
+
+add x: S에 x를 추가한다. (1 ≤ x ≤ 20) S에 x가 이미 있는 경우에는 연산을 무시한다.
+remove x: S에서 x를 제거한다. (1 ≤ x ≤ 20) S에 x가 없는 경우에는 연산을 무시한다.
+check x: S에 x가 있으면 1을, 없으면 0을 출력한다. (1 ≤ x ≤ 20)
+toggle x: S에 x가 있으면 x를 제거하고, 없으면 x를 추가한다. (1 ≤ x ≤ 20)
+all: S를 {1, 2, ..., 20} 으로 바꾼다.
+empty: S를 공집합으로 바꾼다. 
+
+input
+첫째 줄에 수행해야 하는 연산의 수 M (1 ≤ M ≤ 3,000,000)이 주어진다.
+
+둘째 줄부터 M개의 줄에 수행해야 하는 연산이 한 줄에 하나씩 주어진다.
+
+output
+check 연산이 주어질때마다, 결과를 출력한다.
+
+외부자료 참고여부 : X
+
+'''
+
+
+import sys
+
 S=set()
 
-def add(i):
-    S.add(i)
+num=int(sys.stdin.readline().rstrip())
 
-def remove(i):
-    S.remove(i)
-
-def check(i):
-    if i in S:
-        return 1
+for i in range(num):
+    temp=sys.stdin.readline().strip().split()
+    if(temp[0]=='add'):
+        S.add(int(temp[1]))
+    elif(temp[0]=='check'):
+        if int(temp[1]) in S:
+            print(1)
+        else:
+            print(0)
+    elif(temp[0]=='remove'):
+        S.discard(int(temp[1]))
+    elif(temp[0]=='toggle'):
+        if int(temp[1]) in S:
+            S.discard(int(temp[1]))
+        else:
+            S.add(int(temp[1]))
+    elif(temp[0]=='all'):
+        S = set([i for i in range(1, 21)])
     else:
-        return 0
-
-def toggle(i):
-    if i in S:
-        S.remove(i)
-    else:
-        S.add(i)
-
-def empty():
-    S.clear()
-
+        S=set()
